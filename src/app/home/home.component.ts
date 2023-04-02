8f6564907daa34189c2fd3eb6ba5721c3e73b800
Mani Kandan <64264387+mkandan1@users.noreply.github.com>
2023-04-02 08:37:11 +0530

    Bug fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
-
+import { Component, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import axios from 'axios';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent   {
+    constructor(){}
 
+    ngOnInit(){
+      
+    }
+   
+
+    submit(name: HTMLInputElement, company: HTMLInputElement, email: HTMLInputElement, sub: HTMLInputElement, message: HTMLTextAreaElement){
+      console.log(name)
+      axios.post('https://api.emailjs.com/api/v1.0/email/send', {
+        service_id: 'service_6ueb06h',
+        template_id: 'template_fdpv07y',
+        user_id: 'zSy7pGYRxfzKffC5O',
+        template_params: {
+            'username': 'James',
+            'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...',
+            name: name,
+            email: email,
+            company: company,
+            subject: sub,
+            message: message
+        }}).then(res => {
+          console.log(res.data);
+          
+        }).catch(error=>{
+          console.error(error)
+        })
+    }
 }
